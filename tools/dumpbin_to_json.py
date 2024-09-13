3201e8c6d917cc5eef9db898764c7f9a4165deb6
widberg <jaredwidberg@gmail.com>
2024-09-12 20:28:07 -0400

    Fix problem with empty json file

--- tools/dumpbin_to_json.py
+++ tools/dumpbin_to_json.py
@@ -5,6 +5,7 @@ import subprocess
 import sys
 import json
 
+
 def process_output(output):
     symbols = set()
     is_symbols = False
@@ -18,38 +19,46 @@ def process_output(output):
                 is_symbols = False
                 continue
             parts = line.split()
-            if len(parts) >= 7 and len(parts) <= 8 and parts[2] != "UNDEF" and parts[len(parts) - 3] == "External":
+            if (
+                len(parts) >= 7
+                and len(parts) <= 8
+                and parts[2] != "UNDEF"
+                and parts[len(parts) - 3] == "External"
+            ):
                 symbols.add(parts[len(parts) - 1])
     return sorted(list(symbols))
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Generate list of defined symbols in libraries and objects.')
-    parser.add_argument('objs', type=str, nargs='+')
-    parser.add_argument('--dumpbin', type=str, required=True)
-    parser.add_argument('--output', type=argparse.FileType('a+'), required=True)
+    parser = argparse.ArgumentParser(
+        description="Generate list of defined symbols in libraries and objects."
+    )
+    parser.add_argument("objs", type=str, nargs="+")
+    parser.add_argument("--dumpbin", type=str, required=True)
+    parser.add_argument("--output", type=argparse.FileType("a+"), required=True)
 
     args = parser.parse_args()
 
-    cmdline = [args.dumpbin, '/SYMBOLS', *args.objs]
+    cmdline = [args.dumpbin, "/SYMBOLS", *args.objs]
     process = subprocess.run(cmdline, stdout=subprocess.PIPE, text=True)
 
     if process.returncode != 0:
         sys.exit(1)
 
     args.output.seek(0)
-    contents = json.load(args.output)
+    try:
+        contents = json.load(args.output)
+    except json.decoder.JSONDecodeError:
+        contents = None
     symbols = process_output(process.stdout)
     result = []
     for symbol in symbols:
-        result.append({
-            "action": "exclude",
-            "type": "symbol",
-            "name": symbol
-        })
+        result.append({"action": "exclude", "type": "symbol", "name": symbol})
 
     if contents != result:
         args.output.truncate(0)
         json.dump(result, args.output, indent=4)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
