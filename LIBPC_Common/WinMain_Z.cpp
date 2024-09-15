91581d66131b5ba4cf2a646851ddcb6dc98d974c
widberg <jaredwidberg@gmail.com>
2024-09-14 21:47:29 -0400

    Add .clang-format and generate compile_commands.json

--- LIBPC_Common/WinMain_Z.cpp
+++ LIBPC_Common/WinMain_Z.cpp
@@ -1,12 +1,14 @@
 #if !USE_SECUROM
-extern "C" bool __cdecl FUN_008443c0(void) {
+extern "C" bool __cdecl FUN_008443c0(void)
+{
     DELINKFUNCTION(0x008443c0);
     return true;
 }
 #endif
 
 #if !USE_RUNNING_CHECK
-extern "C" unsigned int __cdecl FUN_00843df0(void) {
+extern "C" unsigned int __cdecl FUN_00843df0(void)
+{
     DELINKFUNCTION(0x00843df0);
     return 0;
 }
