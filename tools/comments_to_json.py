5577a6e54b7effbd2e2d6dbe01d6f94ba124c6a2
widberg <jaredwidberg@gmail.com>
2024-10-05 18:29:45 -0400

    Enable LTO

--- tools/comments_to_json.py
+++ tools/comments_to_json.py
@@ -5,52 +5,103 @@ import json
 import struct
 
 OBJ_MAGIC = 0x14C
+MSIL_MAGIC = b"\x00\x00\xFF\xFF\x01\x00\x4C\x01"
 LIB_MAGIC = b"!<arch>\n"
 
 
-def process_obj(obj, start=0, size=None):
-    obj.seek(start + 8)
-    pointer_to_symbol_table = struct.unpack("<I", obj.read(4))[0]
-    num_symbols = struct.unpack("<I", obj.read(4))[0]
-    obj.seek(start + pointer_to_symbol_table)
+def process_obj(f, start=0, size=None):
+    f.seek(start + 8)
+    pointer_to_symbol_table = struct.unpack("<I", f.read(4))[0]
+    num_symbols = struct.unpack("<I", f.read(4))[0]
+    f.seek(start + pointer_to_symbol_table)
     symbol_table_size = num_symbols * 18
-    obj.seek(start + pointer_to_symbol_table + symbol_table_size)
-    string_table_size = struct.unpack("<I", obj.read(4))[0]
-    obj.seek(start + pointer_to_symbol_table + symbol_table_size + string_table_size)
+    f.seek(start + pointer_to_symbol_table + symbol_table_size)
+    string_table_size = struct.unpack("<I", f.read(4))[0]
+    f.seek(start + pointer_to_symbol_table + symbol_table_size + string_table_size)
     if size is None:
-        comments_data = obj.read()
+        comments_data = f.read()
     else:
-        pos = obj.tell()
-        comments_data = obj.read(size - pos + start)
+        pos = f.tell()
+        comments_data = f.read(size - pos + start)
     return comments_data.decode("utf-8").split("\x00")[:-1]
 
 
-def process_lib(lib):
+def process_msil(f, start=0, size=None):
+    f.seek(start + 34)
+    num_sections = struct.unpack("<H", f.read(2))[0]
+    f.seek(start + 52)
+    cil_gl = None
+    for _ in range(num_sections):
+        section_header = f.read(40)
+        if section_header[:7] == b".cil$gl":
+            cil_gl = struct.unpack("<I", section_header[20:24])[0] + 32
+            break
+    if cil_gl is None:
+        return []
+    f.seek(start + cil_gl + 21)
+    trailer = struct.unpack("<I", f.read(4))[0]
+    f.seek(trailer - 25, 1)
+    flags = f.read(2)
+    assert flags[0] == 0x0A
+    if flags[1] == 0x80:
+        f.seek(4, 1)
+        b = f.read(1)
+        while b != b"\x00":
+            b = f.read(1)
+        f.seek(8, 1)
+    f.seek(1, 1)
+    num_options = struct.unpack("<B", f.read(1))[0]
+    if num_options >= 0x80:
+        num_options = struct.unpack("<I", f.read(4))[0]
+    comments = []
+    for _ in range(num_options):
+        type = struct.unpack("<B", f.read(1))[0]
+        value = b""
+        b = f.read(1)
+        while b != b"\x00":
+            value += b
+            b = f.read(1)
+        if type == 6:
+            comments.append(value.decode("utf-8"))
+    if size != None:
+        f.seek(start + size)
+    return comments
+
+
+def process_lib(f):
     comments = []
     while True:
-        header = lib.read(60)
+        header = f.read(60)
         if len(header) < 60:
             break
         size = int(header[48:58].decode("utf-8").strip())
         padding = size % 2
-        start = lib.tell()
-        magic = struct.unpack("<H", lib.read(2))[0]
+        start = f.tell()
+        magic = struct.unpack("<H", f.read(2))[0]
         if magic == OBJ_MAGIC:
-            comments += process_obj(lib, start, size)
-            lib.seek(padding, 1)
+            comments += process_obj(f, start, size)
+            f.seek(padding, 1)
         else:
-            lib.seek(start + size + padding)
+            f.seek(start)
+            magic = f.read(8)
+            if magic == MSIL_MAGIC:
+                comments += process_msil(f, start, size)
+                f.seek(padding, 1)
+            else:
+                f.seek(start + size + padding)
     return comments
 
 
-def process_file(file):
-    magic = struct.unpack("<H", file.read(2))[0]
+def process_file(f):
+    magic = struct.unpack("<H", f.read(2))[0]
     if magic == OBJ_MAGIC:
-        return process_obj(file)
-    file.seek(0)
-    magic = file.read(8)
+        return process_obj(f)
+    f.seek(0)
+    magic = f.read(8)
+    if magic == MSIL_MAGIC:
+        return process_msil(f)
     if magic == LIB_MAGIC:
-        return process_lib(file)
+        return process_lib(f)
     print("Unknown file type.")
     exit(1)
 
@@ -75,17 +126,22 @@ def main():
         parts = comment.split(";")
         if len(parts) == 5:
             tag = parts[0]
-            undname = parts[1] # to check template parameters
+            undname = parts[1]  # to check template parameters
             dname = parts[2]
             address = parts[3]
             template_params = parts[4].split(", ")
             if tag == "delink":
-                if template_params != ['']:
+                if template_params != [""]:
                     print("Template parameters are not supported yet.")
                     exit(1)
                 if address in symbols:
                     if symbols[address] != dname:
-                        print("Symbol address collision:", address, symbols[address], dname)
+                        print(
+                            "Symbol address collision:",
+                            address,
+                            symbols[address],
+                            dname,
+                        )
                         exit(1)
                 else:
                     symbols[address] = dname
