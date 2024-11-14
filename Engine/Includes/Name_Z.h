227bf9d3eb1a39791e13d15fbbd3bc863d33a687
SabeMP <sabemasterprox@gmail.com>
2024-11-14 01:40:27 -0300

    Add Name_Z files, struct and related globals

---
+++ Engine/Includes/Name_Z.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef NAME_Z_H
+#define NAME_Z_H
+
+#include "Types_Z.h"
+
+struct Name_Z
+{
+	static Name_Z Null;
+
+	U32	m_ID;
+}
+
+#endif
---
+++ Engine/Includes/Types_Z.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef TYPES_Z_H
+#define TYPES_Z_H
+
+typedef unsigned __int64 U64;
+typedef __int64 S64;
+typedef char Bool;
+typedef unsigned int U32;
+typedef signed int S32;
+typedef unsigned short U16;
+typedef signed short S16;
+typedef unsigned char U8;
+typedef signed char S8;
+typedef float F32;
+typedef char Char;
+typedef float Float;
+
+#endif
